749b9f8beb9955f14db9c91cbf8977d142cf9008
Nevss <85764621@qq.com>
2018-05-03 19:39:18 +0800

    Update cmdb_install.sh

--- cmdb_install.sh
+++ cmdb_install.sh
@@ -39,9 +39,9 @@ yum group install "Development Tools" -y
 
 sleep 1s 
 
-wget https://github.com/Nevss/bk_cmdb_install/blob/master/redis-3.2.11.tar.gz?raw=true && tar xf redis-3.2.11.tar.gz
-wget https://github.com/Nevss/bk_cmdb_install/blob/master/zookeeper-3.4.12.tar.gz?raw=true && tar xf zookeeper-3.4.11.tar.gz
-wget https://github.com/Nevss/bk_cmdb_install/blob/master/cmdb-3.0.6.tar.gz?raw=true && tar xf cmdb-3.0.6.tar.gz
+wget https://media.githubusercontent.com/media/Nevss/bk_cmdb_install/master/redis-3.2.11.tar.gz?raw=true && tar xf redis-3.2.11.tar.gz
+wget https://media.githubusercontent.com/media/Nevss/bk_cmdb_install/master/zookeeper-3.4.12.tar.gz?raw=true && tar xf zookeeper-3.4.11.tar.gz
+wget https://media.githubusercontent.com/media/Nevss/bk_cmdb_install/master/cmdb-3.0.6.tar.gz?raw=true && tar xf cmdb-3.0.6.tar.gz
 
 wget https://github.com/Nevss/bk_cmdb_install/blob/master/node-v4.5.0.tar.gz?raw=true && tar xf node-v4.5.0.tar.gz
 
